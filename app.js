78bdea1578f5b3ff2ed309e781834f84e2fcbbbd
Adedamola Rioland <riolandadedamola@gmail.com>
2021-10-18 20:35:33 +0100

    make

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // package
-const http=require("http");
-const DATA=require("./ocnfig");
+const DATA = require("./ocnfig");
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
@@ -12,19 +11,16 @@ const db = mysql.createPool({
   host: DATA.HOST,
   user: DATA.USER,
   password: DATA.PASSWORD,
-  port:DATA.PORT,
-  database:DATA.DB,
+  port: DATA.PORT,
+  database: DATA.DB,
 });
 // meddleware
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+
 // get all users
 app.get("/api/users", (req, res) => {
   db.getConnection((err, conn) => {
@@ -34,9 +30,9 @@ app.get("/api/users", (req, res) => {
     }
     let query = "select * from users";
     conn.query(query, (sqlerr, result) => {
-      if(sqlerr){
-       res.status(404).send(sqlerr);
-       return; 
+      if (sqlerr) {
+        res.status(404).send(sqlerr);
+        return;
       }
       res.send(result);
     });
@@ -44,12 +40,12 @@ app.get("/api/users", (req, res) => {
 });
 // get single user
 app.get("/api/users/:uid", (req, res) => {
-  const q="select * from users where uid=?";
-  db.query(q,[req.params.uid],(err,result)=>{
-      if(err){
-        res.status(400).send(err);
-      }
-      res.send(result)
+  const q = "select * from users where uid=?";
+  db.query(q, [req.params.uid], (err, result) => {
+    if (err) {
+      res.status(400).send(err);
+    }
+    res.send(result);
   });
   // res.send(req.params);
 });
@@ -62,48 +58,43 @@ app.post("/api/login", (req, res) => {
     if (result) {
       // res.status(400).send(err);
       res.send(result);
-     }else{
-       res.status(400).send(err);
-     }
-    
+    } else {
+      res.status(400).send(err);
+    }
   });
   // res.send(req.body);
 });
 
 // update pin
- app.put("/api/savepin/:uid",(req,res)=>{
-  const uid=req.params.uid;
+app.put("/api/savepin/:uid", (req, res) => {
+  const uid = req.params.uid;
   const pin = req.body.pin;
- 
+
   const q = "UPDATE users SET pin=? WHERE uid=?";
-  db.query(q,[pin,uid],(err,result)=>{
-    if(err){
+  db.query(q, [pin, uid], (err, result) => {
+    if (err) {
       res.send(err.message);
-    }else {
-      if(result){
-       res.send(result.message);
+    } else {
+      if (result) {
+        res.send(result.message);
       }
     }
-  })
-
- });
+  });
+});
 //create user
 app.post("/api/register", (req, resp) => {
   const value = req.body;
   if (value) {
-   const q = "INSERT INTO `users`(`displayname`, `email`, `token`, `photo`, `conutry`, `time`, `uid`, `phonenumber`, `firstname`, `pin`, `lastname`, `referer`, `biometric`, `password`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-   db.query(q,[],(err,sqlres)=> {
-     if(sqlres){
-       resp.send(sqlres);
-     }
-   });
-
-
+    const q =
+      "INSERT INTO `users`(`displayname`, `email`, `token`, `photo`, `conutry`, `time`, `uid`, `phonenumber`, `firstname`, `pin`, `lastname`, `referer`, `biometric`, `password`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    db.query(q, [], (err, sqlres) => {
+      if (sqlres) {
+        resp.send(sqlres);
+      }
+    });
   }
 
   resp.send(param);
 });
-const server = http.createServer(app);
-server.listen(port, () => {
-  console.log(`working on port ${port}`);
-});
+
+
