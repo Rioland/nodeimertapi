c249821b33e336b3c59859e5902210081fffcb07
Adedamola Rioland <riolandadedamola@gmail.com>
2021-10-18 19:51:52 +0100

    make

---
+++ id16408694_riotech.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 15, 2021 at 01:24 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id16408694_riotech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `sn` int(11) NOT NULL,
+  `displayname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `conutry` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `time` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT current_timestamp(),
+  `uid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phonenumber` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `firstname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pin` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `referer` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `biometric` tinyint(1) NOT NULL DEFAULT 0,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`sn`, `displayname`, `email`, `token`, `photo`, `conutry`, `time`, `uid`, `phonenumber`, `firstname`, `pin`, `lastname`, `referer`, `biometric`, `password`) VALUES
+(1, 'Rioland', 'rioland@gmail.com', NULL, NULL, NULL, 'current_timestamp()', '123456789', NULL, NULL, NULL, NULL, NULL, 0, '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Wallets`
+--
+
+CREATE TABLE `Wallets` (
+  `sn` int(11) NOT NULL,
+  `uid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `dollars` double NOT NULL,
+  `ethereum` double NOT NULL,
+  `litecoin` double NOT NULL,
+  `dogecoin` double NOT NULL,
+  `bitcoincash` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`sn`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `uid` (`uid`);
+
+--
+-- Indexes for table `Wallets`
+--
+ALTER TABLE `Wallets`
+  ADD PRIMARY KEY (`sn`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Wallets`
+--
+ALTER TABLE `Wallets`
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ ocnfig.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "id16408694_rio",
+  password: "8{pHs=CIscWDeF6$",
+  database: "id16408694_riotech",
+});
+
+
+
+module.exports = {
+  HOST: "localhost",
+  USER: "id16408694_rio",
+  PASSWORD: "8{pHs=CIscWDeF6$",
+  DB: "id16408694_riotech",
+};
+// 000_SPECIAL_OFFER
