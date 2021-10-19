6a104e631c1868086a5ded55fa6886410e7af429
Adedamola Rioland <riolandadedamola@gmail.com>
2021-10-19 07:04:54 +0100

    make it better

--- ocnfig.js
+++ ocnfig.js
@@ -3,22 +3,22 @@ const mysql = require("mysql");
 
 
 
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "id16408694_rio",
-//   PASSWORD: "3dTIeD[q9my}+g$%",
-//   DB: "id16408694_riotech",
-//   PORT: "3306",
-// };
-
-
 module.exports = {
-  HOST: "sql5.freesqldatabase.com",
-  USER: "sql5440458",
-  PASSWORD: "x9MQpxLtRV",
-  DB: "sql5440458",
+  HOST: "localhost",
+  USER: "id16408694_rio",
+  PASSWORD: "3dTIeD[q9my}+g$%",
+  DB: "id16408694_riotech",
   PORT: "3306",
 };
+
+
+// module.exports = {
+//   HOST: "sql5.freesqldatabase.com",
+//   USER: "sql5440458",
+//   PASSWORD: "x9MQpxLtRV",
+//   DB: "sql5440458",
+//   // PORT: "3306",
+// };
 
 // module.exports = {
 //   HOST: "localhost",
